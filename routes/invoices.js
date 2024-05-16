6dd7fa8704f63aa79bbdef4e6f953ef2a01757e0
Jacob <jacobrashid404@gmail.com>
2024-05-16 16:35:38 -0700

    GET /:id invoice route

--- routes/invoices.js
+++ routes/invoices.js
@@ -18,6 +18,7 @@ router.get("/", async function (req, res) {
   return res.json({ invoices });
 });
 
+
 /** Get requested invoice.
  *  If invoice cannot be found, returns 404.
  *  Returns
@@ -26,13 +27,36 @@ router.get("/", async function (req, res) {
  * } */
 router.get("/:id", async function (req, res) {
   const id = req.params.id;
-
-  // first query invoices db to get the comp_code (FK for companies)
+  
+  // include comp_code in invoice query for later company query
+  // comp_code key will be removed before final return
   const invoiceResults = await db.query(`
     SELECT id, comp_code, amt, paid, add_date, paid_date
     FROM invoices
-    WHERE id = $1`, [id]);
+    WHERE id = $1`, [id]
+  );
   const invoice = invoiceResults.rows[0];
+  if (!invoice) throw new NotFoundError(`No matching invoice id: ${id}`);
+  
+  // get company code from invoice query
+  const companyCode = invoice.comp_code;
+  
+  // delete company code key from invoice
+  // company details get added from company query below
+  delete invoice.comp_code;
+  
+  // select company by code received from invoice query
+  const companyResults = await db.query(`
+    SELECT code, name, description
+    FROM companies
+    WHERE code = $1`, [companyCode]
+  );
+  const company = companyResults.rows[0];
+  
+  // add company key to invoice with company details as its value
+  invoice.company = company;
+  
+  return res.json({ invoice });
 });
 
 
