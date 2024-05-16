8f75ef07d1f8ec31f48294a2eef40b53db1c395d
Jacob <jacobrashid404@gmail.com>
2024-05-16 12:35:11 -0700

    created biztime db

--- db.js
+++ db.js
@@ -1,2 +1,16 @@
 /** Database setup for BizTime. */
 
+import pg from "pg";
+const { Client } = pg;
+
+const DB_URI = process.env.NODE_ENV === "test"  // 1
+    ? "postgresql:///biztime_test"
+    : "postgresql:///biztime";
+
+const db = new Client({
+  connectionString: DB_URI,
+});
+
+await db.connect();                             // 2
+
+export default db; 
