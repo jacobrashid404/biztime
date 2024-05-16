e4ee0a8fe6eb297a48d789feae71c92e7d4df249
Jacob <jacobrashid404@gmail.com>
2024-05-16 15:25:21 -0700

    created invoices routes file

---
+++ routes/invoices.js
@@ -0,0 +1,9 @@
+import express from "express";
+import db from "../db.js";
+import { BadRequestError, NotFoundError } from "../expressError.js";
+
+const router = new express.Router();
+
+
+
+export default router;
